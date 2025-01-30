611d1a6e36faf628d4472304775b8ff3437c5893
Fikayomi31 <fikayoogundijo@gmail.com>
2025-01-30 04:37:11 -0800

    update app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from flask import Flask, jsonify, Response
 from datetime import datetime
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Define the data to be returned
 def get_data():
     return {
         "email": "fikayoogundijo@gmail.com",
-        "datetime": datetime.utcnow().isoformat() + "Z",
-        "github_repo_url": "https://github.com/Fikayoogundijo/hng-stage0-api",
+        "datetime": datetime.utcnow().isoformat(),
+        "github_repo_url": "https://github.com/Fikayomi31/HNG12_TASK_0",
     }
 
 # Define the API endpoint
 @app.route('/api', methods=['GET'])
 def api():
     data = get_data()
-    # Pretty-print JSON with indentation and ensure email is first
+    # Print JSON with indentation and ensure email is first
     response = json.dumps(data, indent=4, sort_keys=False)
     return Response(response, mimetype='application/json')
 
-    return jsonify(get_data())
 
 # Run the server
 if __name__ == '__main__':
